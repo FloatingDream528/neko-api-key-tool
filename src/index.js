63fa18906f00bef5aa696dc981c147bea7d73a67
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2026-02-22 09:52:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Layout } from '@douyinfe/semi-ui';
+
 import App from './App';
 import HeaderBar from './components/HeaderBar';
 import reportWebVitals from './reportWebVitals';
@@ -9,28 +10,24 @@ import './index.css';
 import { ThemeProvider } from './context/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const { Sider, Content, Header } = Layout;
+const { Content, Header } = Layout;
+
 root.render(
-  <ThemeProvider>
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header style={{ position: 'sticky', top: 0, zIndex: 1 }}>
-        <HeaderBar />
-      </Header>
-      <Layout>
-        <Content style={{ 
-          padding: '24px',
-          '@media screen and (max-width: 768px)': {
-            padding: '12px'
-          }
-        }}>
+  <React.StrictMode>
+    <ThemeProvider>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header style={{ padding: 0 }}>
+          <HeaderBar />
+        </Header>
+        <Content style={{ padding: 16 }}>
           <App />
         </Content>
       </Layout>
-    </Layout>
-  </ThemeProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint.
 reportWebVitals();
