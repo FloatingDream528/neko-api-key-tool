3e3ce33a28dfcbcc3e7feeadfc714e2b10bebd0c
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2026-02-22 09:56:50 +0800

    Update HeaderBar.js

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,10 +1,6 @@
 import { Nav } from '@douyinfe/semi-ui';
 import { IconTag } from '@douyinfe/semi-icons-lab';
-import {
-  IconGithubLogo,
-  IconMoon,
-  IconSun,
-} from '@douyinfe/semi-icons';
+import { IconGithubLogo, IconMoon, IconSun } from '@douyinfe/semi-icons';
 import { useSetTheme, useTheme } from '../context/Theme';
 
 const HeaderBar = () => {
@@ -12,52 +8,43 @@ const HeaderBar = () => {
   const setTheme = useSetTheme();
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        height: '100%',
+    <Nav
+      mode="horizontal"
+      header={{
+        logo: <IconTag size="large" />,
+        text: 'Neko API Key Tool',
       }}
-    >
-      <Nav
-        mode='horizontal'
-        header={
-          {
-            text: '令牌查询',
-            logo: (
-              <div style={{ width: '100%', height: '100%' }}>
-                < IconTag size='large' />
-              </div>
-            )
-          }
-        }
-        footer={
-          <>
-            {theme === 'dark' ? (
-              <IconSun
-                size="large"
-                style={{ cursor: 'pointer', marginRight: '10px', color: 'var(--semi-color-text-2)' }}
-                onClick={() => setTheme(false)}
-              />
-            ) : (
-              <IconMoon
-                size="large"
-                style={{ cursor: 'pointer', marginRight: '10px', color: 'var(--semi-color-text-2)' }}
-                onClick={() => setTheme(true)}
-              />
-            )}
-            {process.env.REACT_APP_SHOW_ICONGITHUB === "true" && (
-              <IconGithubLogo
-                size="large"
-                style={{ cursor: 'pointer', marginRight: '10px', color: 'var(--semi-color-text-2)' }}
-                onClick={() => window.open('https://github.com/Calcium-Ion/neko-api-key-tool', '_blank')}
-              />
-            )}
-          </>
-        }
-      />
-    </div>
+      footer={
+        <>
+          {theme === 'dark' ? (
+            <Nav.Item
+              icon={<IconSun />}
+              text="Light"
+              onClick={() => setTheme(false)}
+            />
+          ) : (
+            <Nav.Item
+              icon={<IconMoon />}
+              text="Dark"
+              onClick={() => setTheme(true)}
+            />
+          )}
+
+          {process.env.REACT_APP_SHOW_ICONGITHUB === 'true' && (
+            <Nav.Item
+              icon={<IconGithubLogo />}
+              text="GitHub"
+              onClick={() =>
+                window.open(
+                  'https://github.com/Calcium-Ion/neko-api-key-tool',
+                  '_blank'
+                )
+              }
+            />
+          )}
+        </>
+      }
+    />
   );
 };
 
